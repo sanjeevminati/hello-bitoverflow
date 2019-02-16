7809747566cbcd1d1a163fefae763f508cd3678e
sanjeevminati <43167327+sanjeevminati@users.noreply.github.com>
2019-02-16 12:31:25 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main() {
+	
+	printf("hello bitoverflow");
+	
+
+getch();
+
+}
